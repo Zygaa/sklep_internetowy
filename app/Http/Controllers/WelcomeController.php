6d312c8783bd526d9730b26359f0428b2160e979
Zyga <patrykzygmunt10@gmail.com>
2022-06-08 17:11:18 +0200

    dodanie kategorii do strony głównej

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
+use App\Models\ProductCategory;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
@@ -20,7 +21,8 @@ class WelcomeController extends Controller
     public function index()
     {
         return view("welcome", [
-          'products' => Product::paginate(10)
+          'products' => Product::paginate(10),
+          'categories' => ProductCategory::orderBy('name' ,'ASC')->get()
         ]);
     }
 }
