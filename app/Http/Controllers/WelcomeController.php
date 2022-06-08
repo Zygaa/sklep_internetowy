32842d8c48ccd0f2ffd14c92204d1d349251fec3
Zyga <patrykzygmunt10@gmail.com>
2022-06-08 20:47:21 +0200

    filtrowanie po kategoriach na stronie głównej

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,20 +9,39 @@ use App\Models\ProductCategory;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Contracts\View\View;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Response;
 use Exception;
 
 class WelcomeController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
+        $filters = $request->query('filter');
+        $query = Product::query();
+        if(!is_null($filters)){
+          if(array_key_exists('categories', $filters)){
+            $query = $query->whereIn('category_id', $filters['categories']);
+          }
+          //$query = $query->whereIn('category_id', $filters['categories']);
+          if(!is_null($filters['price_min'])){
+            $query = $query->where('price','>=', $filters['price_min']);
+          }
+          if(!is_null($filters['price_max'])){
+            $query = $query->where('price','<=', $filters['price_max']);
+          }
+          return response()->json([
+            'data' => $query->get()
+          ]);
+        }
         return view("welcome", [
-          'products' => Product::paginate(10),
-          'categories' => ProductCategory::orderBy('name' ,'ASC')->get()
+          'products' => $query->paginate(10),
+          'categories' => ProductCategory::orderBy('name' ,'ASC')->get(),
+          'defaultImage' => 'https://via.placeholder.com/240x240/5fa9f8/efefef'
         ]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
             </div>
           </div>
         </div>
-        <div class="row">
+        <div class="row" id="products-wrapper">
           @foreach($products as $product)
             <div class="col-6 col-md-6 col-lg-4 mb-3">
               <div class="card h-100 border-0">
@@ -41,7 +41,7 @@
                   @if(!is_null($product->image_path))
                     <img src="{{ asset('storage/' .$product->image_path) }}" class="img-fluid mx-auto d-block" alt="Zdjęcie produktu">
                   @else
-                    <img src="https://via.placeholder.com/240x240/5fa9f8/efefef" class="img-fluid mx-auto d-block" alt="Zdjęcie produktu">
+                    <img src="{{ $defaultImage }}" class="img-fluid mx-auto d-block" alt="Zdjęcie produktu">
                   @endif
                 </div>
                 <div class="card-body text-center">
@@ -80,13 +80,13 @@
         </div>
       </div>
     </div>
-    <div class="col-md-4 order-md-1 col-lg-3 sidebar-filter">
+    <form class="col-md-4 order-md-1 col-lg-3 sidebar-filter">
       <h3 class="mt-0 mb-5">Produkty <span class="text-primary">{{ count($products) }}</span></h3>
       <h6 class="text-uppercase font-weight-bold mb-3">Kategorie</h6>
       @foreach($categories as $category)
         <div class="mt-2 mb-2 pl-2">
           <div class="custom-control custom-checkbox">
-            <input type="checkbox" class="custom-control-input" id="category-{{ $category->id }}">
+            <input type="checkbox" class="custom-control-input" name="filter[categories][]" id="category-{{ $category->id }}" value="{{ $category->id }}">
             <label class="custom-control-label" for="{{ $category->id }}">{{ $category->name }}</label>
           </div>
         </div>
@@ -94,15 +94,57 @@
       <div class="divider mt-5 mb-5 border-bottom border-secondary"></div>
       <h6 class="text-uppercase mt-5 mb-3 font-weight-bold">Cena</h6>
       <div class="price-filter-control">
-        <input type="number" class="form-control w-50 pull-left mb-2" value="50" id="price-min-control">
-        <input type="number" class="form-control w-50 pull-right" value="150" id="price-max-control">
+        <input type="number" class="form-control w-50 pull-left mb-2" placeholder="50" name="filter[price_min]" id="price-min-control">
+        <input type="number" class="form-control w-50 pull-right" placeholder="150" name="filter[price_max]" id="price-max-control">
       </div>
       <input id="ex2" type="text" class="slider " value="50,150" data-slider-min="10" data-slider-max="200" data-slider-step="5" data-slider-value="[50,150]" data-value="50,150" style="display: none;">
       <div class="divider mt-5 mb-5 border-bottom border-secondary"></div>
-      <a href="#" class="btn btn-lg btn-block btn-primary mt-5">Odśwież wyniki</a>
-    </div>
-
+      <a href="#" class="btn btn-lg btn-block btn-primary mt-5" id="filter-button">Odśwież wyniki</a>
+    </form>
   </div>
 </div>
-
+@endsection
+@section('javascript')
+    const storagePath = '{{ asset('storage') }}/';
+    const defaultImage = '{{ $defaultImage }}';
+    $(function(){
+      $('a#filter-button').click(function(){
+        const form = $('form.sidebar-filter').serialize();
+        $.ajax({
+          method: "GET",
+          url: "/",
+          data: form
+        })
+        .done(function (response){
+          $('div#products-wrapper').empty();
+          $.each(response.data, function(index, product){
+            const html = '<div class="col-6 col-md-6 col-lg-4 mb-3">' +
+                    '            <div class="card h-100 border-0">' +
+                    '                <div class="card-img-top">' +
+                    '                    <img src="' + getImage(product) + '" class="img-fluid mx-auto d-block" alt="Zdjęcie produktu">' +
+                    '                </div>' +
+                    '                <div class="card-body text-center">' +
+                    '                    <h4 class="card-title">' +
+                                            product.name +
+                    '                    </h4>' +
+                    '                    <h5 class="card-price small">' +
+                    '                        <i>PLN ' + product.price + '</i>' +
+                    '                    </h5>' +
+                    '                </div>' +
+                    '            </div>' +
+                    '        </div>';
+                $('div#products-wrapper').append(html);
+          });
+        })
+        .fail(function (data){
+          Swal.fire('ERROR');
+        });
+      });
+      function getImage(product){
+        if(!!product.image_path){
+          return storagePath + product.image_path;
+        }
+        return defaultImage;
+      }
+    });
 @endsection
